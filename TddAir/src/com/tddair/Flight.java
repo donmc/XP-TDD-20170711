6b92b313f337e0624eb7b829d27ef02d825a4200
Don McGreal <mail@donmcgreal.com>
2017-07-10 16:06:48 -0500

    Added CheckStyle

--- TddAir/src/com/tddair/Flight.java
+++ TddAir/src/com/tddair/Flight.java
@@ -1,50 +1,65 @@
 package com.tddair;
 
-
 public class Flight {
-	
-	private String origin;
-	private String destination;
-	private String airline;
-	private int number;
-	private int mileage;
 
-	public Flight(String origin, String destination, int mileage, String airline, int number) {
-		if(origin == null || origin.length() != 3) {
-			throw new IllegalArgumentException("Invalid origin code");
-		}
-		if(destination == null || destination.length() != 3) {
-			throw new IllegalArgumentException("Invalid destination code");
-		}
-		if(mileage < 100) {
-			throw new IllegalArgumentException("Mileage muse be greater than 100");
-		}
-		this.origin = origin;
-		this.destination = destination;
-		this.mileage = mileage;
-		this.airline = airline;
-		this.number = number;
-	}
-	
-	public Flight(String origin, String destination, int mileage) {
-		this(origin, destination, mileage, null, 0);
-	}
-	
-	public String getFullFlightNumber() {
-		if (airline == null || number == 0) {
-			return "UNKNOWN";
-		}
-		return airline + number;
-	}
-	
-	public String getDestination() {
-		return destination;
-	}
-	public String getOrigin() {
-		return origin;
-	}
-	public int getMileage() {
-		return mileage;
-	}
-	
+  private String origin;
+  private String destination;
+  private String airline;
+  private int number; 
+  private int mileage;
+
+  /**
+   * Constructor to use when flight number and airline is known.
+   * 
+   * @param origin the three letter airline code for origin airport
+   * @param destination the three letter airline code for destination airport
+   * @param mileage the miles between origin and destination
+   * @param airline the two letter airline code
+   * @param number the numbered flight number
+   */
+  public Flight(String origin, String destination, int mileage, String airline, int number) {
+    if (origin == null || origin.length() != 3) {
+      throw new IllegalArgumentException("Invalid origin code");
+    }
+    if (destination == null || destination.length() != 3) {
+      throw new IllegalArgumentException("Invalid destination code");
+    }
+    if (mileage < 100) {
+      throw new IllegalArgumentException("Mileage must be greater than 100");
+    }
+    this.origin = origin;
+    this.destination = destination;
+    this.mileage = mileage;
+    this.airline = airline;
+    this.number = number;
+  }
+
+  public Flight(String origin, String destination, int mileage) {
+    this(origin, destination, mileage, null, 0);
+  }
+
+  /**
+   * Concatenates the airline code with the flight number to produce a full flight number.
+   * 
+   * @return the complete flight number (airline code + flight number)
+   */
+  public String getFullFlightNumber() {
+    if (airline == null || number == 0) {
+      return "UNKNOWN";
+    }
+    return airline + number;
+  }
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public String getOrigin() {
+    return origin;
+  }
+
+  public int getMileage() {
+    return mileage;
+  }
+
 }
--- TddAir/src/com/tddair/TddAirApplication.java
+++ TddAir/src/com/tddair/TddAirApplication.java
@@ -1,14 +1,23 @@
 package com.tddair;
 
-
 public class TddAirApplication {
-	
-	private FlightDao flights = new FlightDao();
-	
-	public TddAirApplication() {
-	}
-	
-	public void addFlight(String origin, String destination, int mileage, String airline, int number) {
-		flights.addFlight(origin, destination, mileage, airline, number);
-	}
+
+  private FlightDao flights = new FlightDao();
+
+  public TddAirApplication() {
+  }
+
+  /**
+   * Adds a flight to the system.
+   * 
+   * @param origin the three letter airline code for origin airport
+   * @param destination the three letter airline code for destination airport
+   * @param mileage the miles between origin and destination
+   * @param airline the two letter airline code
+   * @param number the numbered flight number
+   */
+  public void addFlight(String origin, String destination, 
+                        int mileage, String airline, int number) {
+    flights.addFlight(origin, destination, mileage, airline, number);
+  }
 }
