fd74244990fa03c4f1a5ac4c016c3e5bf59c5cc8
ghosht3 <ghosht3@USENGHOSHT3L1C.corp.emc.com>
2017-07-11 11:45:31 -0500

    Added testcases for flight class

---
+++ TddAir/test/com/tddair/FlightTest.java
@@ -0,0 +1,43 @@
+package com.tddair;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlightTest {
+
+	@Test
+	public void testGetFullFlightNumber_Success() {
+		//Setup
+		Flight flight = new Flight("DFW","ORD",980,"AA",234);		
+		//Execute
+		String flightNum = flight.getFullFlightNumber();
+		//Verify
+		assertEquals("AA234", flightNum);
+		//Teardown
+	}
+	
+	
+	@Test
+	public void testGetFullFlightNumber_NoFlightNo() {
+		//Setup
+		Flight flight = new Flight("DFW","ORD",980,"AA",0);		
+		//Execute
+		String flightNum = flight.getFullFlightNumber();
+		//Verify
+		assertEquals("UNKNOWN", flightNum);
+		//Teardown
+	}
+	
+	@Test
+	public void testGetFullFlightNumber_NullAirline() {
+		//Setup
+		Flight flight = new Flight("DFW","ORD",980,null,234);		
+		//Execute
+		String flightNum = flight.getFullFlightNumber();
+		//Verify
+		assertEquals("UNKNOWN", flightNum);
+		//Teardown
+	}
+
+}
