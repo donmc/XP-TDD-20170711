f8752c2b425a961d1957c5e29a3da9a3a978e31a
ragups <srini.ragupathy@vce.com>
2017-07-12 12:50:53 -0500

    Status REd TEst

--- TddAir/src/com/tddair/Member.java
+++ TddAir/src/com/tddair/Member.java
@@ -2,4 +2,9 @@ package com.tddair;
 
 public class Member {
 
+	public Object getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
--- TddAir/test/com/tddair/WhenRegisteringMember.java
+++ TddAir/test/com/tddair/WhenRegisteringMember.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class WhenRegisteringMember {
 
 	@Test
-	public void shouldRegisterSuccessfully() {
+	public void shouldRegisterSuccessfullyTestNotNull() {
 		//set up
 		TddAirApplication app = new TddAirApplication();
 		
@@ -22,5 +22,24 @@ public class WhenRegisteringMember {
         assertNotNull(member);
 		//fail("Not yet implemented");
 	}
+	
+	@Test
+	public void shouldRegisterSuccessfullyStatusCheck() {
+		//set up
+		TddAirApplication app = new TddAirApplication();
+		
+		String userName = "john";
+	    String email = "john@aa.com";
+	    app.registerMember(userName,email);
+	    
+	    //execute
+	    Member member = app.lookupMember(userName); 
+	    
+	    	    //validate
+        assertEquals("RED", member.getStatus());
+		//fail("Not yet implemented");
+	}
+	
+	
 
 }
