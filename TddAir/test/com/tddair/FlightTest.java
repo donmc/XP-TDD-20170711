857c6426999248201474941d026548b228135837
ghantk1 <kiran.ghanta@dell.com>
2017-07-12 13:06:40 -0500

    Test Changes - Kiran

--- TddAir/test/com/tddair/FlightTest.java
+++ TddAir/test/com/tddair/FlightTest.java
@@ -10,60 +10,59 @@ public class FlightTest {
 		Flight flight = new Flight("DFW", "ORD", 980, "AA", 234);
 		assertEquals("DFW", flight.getOrigin());
 		assertEquals(980, flight.getMileage());
-		
+
 	}
-	
+
 	@Test
 	public void testFlightCreation_InvalidOriginLengthn() {
 		try {
-		new Flight("DFTW", "ORD", 980, "AA", 234);
-		fail("Should throw Illegal Exception");
-		}catch(IllegalArgumentException e) {
+			new Flight("DFTW", "ORD", 980, "AA", 234);
+			fail("Should throw Illegal Exception");
+		} catch (IllegalArgumentException e) {
 			assertEquals("Invalid origin code", e.getMessage());
 		}
-		
+
 	}
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testFlightCreation_NullOrigin() {
 		new Flight("DFW", "ORD", 980, "AA", 234);
-		
-		
+
 	}
-	
+
 	@Test
 	public void testGetFullFlightNumber() {
-		//Setup
+		// Setup
 		Flight flight = new Flight("DFW", "ORD", 980, "AA", 234);
-		
-		
-		//Execute
-		String flightNo =flight.getFullFlightNumber();
-		//Verify
+
+		// Execute
+		String flightNo = flight.getFullFlightNumber();
+		// Verify
 		assertEquals("AA234", flightNo);
-		
+
 	}
+
 	@Test
 	public void testGetFullFlightNumber_NullAirline() {
-		//Setup
+		// Setup
 		Flight flight = new Flight("DFW", "ORD", 980, null, 234);
-		
-		
-		//Execute
-		String flightNo =flight.getFullFlightNumber();
-		//Verify
+
+		// Execute
+		String flightNo = flight.getFullFlightNumber();
+		// Verify
 		assertEquals("UNKNOWN", flightNo);
-		
+
 	}
+
 	@Test
 	public void testGetFullFlight_NoFlightNumber() {
-		//Setup
+		// Setup
 		Flight flight = new Flight("DFW", "ORD", 980, "AA", 0);
-		
-		
-		//Execute
-		String flightNo =flight.getFullFlightNumber();
-		//Verify
+
+		// Execute
+		String flightNo = flight.getFullFlightNumber();
+		// Verify
 		assertEquals("UNKNOWN", flightNo);
-		
+
 	}
 }
