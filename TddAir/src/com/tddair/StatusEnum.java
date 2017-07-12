4306b8fa3e12ba8db447ac61fb038fde08cc2675
srinip6 <priya.srinivasan2@vce.com>
2017-07-12 16:11:07 -0500

    Status Enum Refactor - to avoid multiple If.

--- TddAir/src/com/tddair/StatusEnum.java
+++ TddAir/src/com/tddair/StatusEnum.java
@@ -1,19 +1,24 @@
 package com.tddair;
 
 public enum StatusEnum {
-	Red, Green, Blue, Gold;
+	Gold (75000), Blue (50000),Green(25000), Red (0);
+	
+	private int threshold;
+	
+	private StatusEnum(int threshold){
+		this.threshold = threshold;
+	}
 
 	public static StatusEnum calculateStatus(int ytdMiles) {
-		StatusEnum status = StatusEnum.Red;
-		if(ytdMiles >= 75000){
-			status =  StatusEnum.Gold;
-		}else if (ytdMiles >= 50000){
-			status =  StatusEnum.Blue;
-		}else if(ytdMiles >=25000){
-			status = StatusEnum.Green;
+		StatusEnum finalStatusEnum = StatusEnum.Red;
+		for (StatusEnum status: StatusEnum.values()) {
+			if(ytdMiles >= status.threshold){
+				finalStatusEnum = status;
+				return finalStatusEnum;
+			}
 		}
+		return finalStatusEnum;
 		
-		return status;
 	}
 
 }
