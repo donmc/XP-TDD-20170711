d9bc525de578518ab926be721f1f621ec8fb9fda
Srinivasaraju Datla <Srinivasaraju.Datla@vce.com>
2017-07-12 14:58:54 -0500

    finished testcase

--- TddAir/src/com/tddair/Member.java
+++ TddAir/src/com/tddair/Member.java
@@ -1,9 +1,40 @@
 package com.tddair;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
+	
+	private String userName;
+	private String emailId;
 	private String status;
 	private int miles;
 	private int balance;
+	private List<Flight> flightList;
+	
+	public Member(String userName,String emailId) {
+		this.userName = userName;
+		this.emailId = emailId;
+		flightList = new ArrayList<>();
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+
 	
 	public String getStatus() {
 		return status;
@@ -27,6 +58,11 @@ public class Member {
 
 	public void setBalance(int balance) {
 		this.balance = balance;
+	}
+
+	public void addFlight(Flight flight) {
+		// TODO Auto-generated method stub
+		
 	}
 
 
